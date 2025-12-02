4bd2663806f5b3089e8853ec777acf955e5587fb
Jacob Reckhard <jacobreckhard@gmail.com>
2025-12-01 19:24:16 -0700

    fixes

--- src/help_screen.rs
+++ src/help_screen.rs
@@ -91,10 +91,12 @@ impl Screen for HelpScreen {
         let controls_left = [
             ("Movement", ""),
             ("Walk", "WASD  or  Arrow Keys"),
+            ("Climb", "Hold shift"),
+            ("Jump", "Space"),
             ("", ""),
             ("Actions", ""),
             ("Left Hand", "Left Click (hold)"),
-            ("Right Hand", "Right Click"),
+            ("Right Hand", "Right Click  (hold)"),
         ];
 
         let controls_right = [
