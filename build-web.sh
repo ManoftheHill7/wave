695494fc7642701d7ca3117b59b8072f8f9d5a55
Jacob Reckhard <jacobreckhard@gmail.com>
2025-12-21 23:54:29 -0700

    fixed my build script

--- build-web.sh
+++ build-web.sh
@@ -22,8 +22,14 @@ echo "Building for WebAssembly..."
 # Get absolute path to storage library
 STORAGE_LIB="$(cd "$(dirname "$0")" && pwd)/web/storage_library.js"
 
-# Set EMCC_CFLAGS for raylib-sys compilation
-export EMCC_CFLAGS="-O3 -sUSE_GLFW=3 -sASSERTIONS=1 -sWASM=1 -sASYNCIFY -sGL_ENABLE_GET_PROC_ADDRESS=1 --js-library=$STORAGE_LIB"
+# Set EMCC_CFLAGS for raylib-sys compilation (compiler flags only)
+export EMCC_CFLAGS="-O3"
+
+# Set linker flags separately (these are passed during final linking, not compilation)
+export EMCC_LINKER_FLAGS="-sUSE_GLFW=3 -sASSERTIONS=1 -sWASM=1 -sASYNCIFY -sGL_ENABLE_GET_PROC_ADDRESS=1 --js-library=$STORAGE_LIB"
+
+# Pass linker flags via RUSTFLAGS for the emscripten target
+export RUSTFLAGS="-C link-args=-sUSE_GLFW=3 -C link-args=-sASSERTIONS=1 -C link-args=-sWASM=1 -C link-args=-sASYNCIFY -C link-args=-sGL_ENABLE_GET_PROC_ADDRESS=1 -C link-args=--js-library=$STORAGE_LIB"
 
 # Build for WASM with cargo
 cargo build --target wasm32-unknown-emscripten --release
